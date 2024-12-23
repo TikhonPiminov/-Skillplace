f1d567e0dcb2cf15924f3b559b11e6657dc4f9bf
TikhonPiminov <tihon.piminov@yandex.com>
2024-12-23 18:31:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99 > 9.98,1000!=1000.1)
+
+#3rd program
+example0 = 2*2+2
+example1 = 2*(2+2)
+print(example0 == example1)
+
+#4th program
+number = format('123.456')
+number = 123.456 * 10
+print(int(number) % 10)
