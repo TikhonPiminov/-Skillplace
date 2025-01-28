44834d7c200c3902483204034eae3e8ed06ac684
TikhonPiminov <tihon.piminov@yandex.com>
2025-01-28 18:10:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 print(9**0.5*5)
 
 #2nd program
-print(9.99 > 9.98,1000!=1000.1)
+print(9.99 > 9.98 and 1000!=1000.1)
 
 #3rd program
 example0 = 2*2+2
@@ -10,6 +10,6 @@ example1 = 2*(2+2)
 print(example0 == example1)
 
 #4th program
-number = format('123.456')
-number = 123.456 * 10
-print(int(number) % 10)
+number ='123.456'
+number = float('123.456') * 10
+print(int(number % 10))
